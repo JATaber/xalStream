56c2c4ae3d63064cb7558e2e8f0278ee7e57901e
James Taber <JATaber@fullsail.edu>
2018-07-28 22:35:58 -0500

    working on commands page

--- xalbiaSite/src/components/Commands.vue
+++ xalbiaSite/src/components/Commands.vue
@@ -1,5 +1,91 @@
 <template>
   <div class="container">
+    <h2>Chat Commands</h2>
+    <div class="jumbotron">
+      <table class="table table-striped">
+        <thead>
+        <h3>Theme Songs</h3>
+        <tr>
+          <th>Command</th>
+          <th>Hint</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(schedule, index) in themes" :key="index">
+          <td>{{ schedule.command }}</td>
+          <td>{{ schedule.hint }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="jumbotron">
+      <table class="table table-striped">
+        <thead>
+        <h3>Warning Sounds</h3>
+        <tr>
+          <th>Command</th>
+          <th>Hint</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(schedule, index) in warnings" :key="index">
+          <td>{{ schedule.command }}</td>
+          <td>{{ schedule.hint }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="jumbotron">
+      <table class="table table-striped">
+        <thead>
+        <h3>Songs</h3>
+        <tr>
+          <th>Command</th>
+          <th>Hint</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(schedule, index) in songs" :key="index">
+          <td>{{ schedule.command }}</td>
+          <td>{{ schedule.hint }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="jumbotron">
+      <table class="table table-striped">
+        <thead>
+        <h3>Celebration Sounds</h3>
+        <tr>
+          <th>Command</th>
+          <th>Hint</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(schedule, index) in celebrate" :key="index">
+          <td>{{ schedule.command }}</td>
+          <td>{{ schedule.hint }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="jumbotron">
+      <table class="table table-striped">
+        <thead>
+        <h3>Sound Effects</h3>
+        <tr>
+          <th>Command</th>
+          <th>Hint</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(schedule, index) in sounds" :key="index">
+          <td>{{ schedule.command }}</td>
+          <td>{{ schedule.hint }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
--- xalbiaSite/src/components/Navbar.vue
+++ xalbiaSite/src/components/Navbar.vue
@@ -1,19 +1,22 @@
 <template>
-  <b-navbar class="nav" toggleable="md">
-    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-    <b-navbar-brand href="/">
-      <img class="logo" src="../assets/images/Profile.png" alt="logo">
-    </b-navbar-brand>
-    <b-collapse is-nav id="nav_collapse">
-      <b-navbar-nav pills>
-        <b-nav-item id="item" :class="{ 'active': $route.path == '/about' }" href="/about">About</b-nav-item>
-        <b-nav-item id="item" :class="{ 'active': $route.path == '/schedule' }" href="/schedule">Schedule</b-nav-item>
-        <b-nav-item id="item" :class="{ 'active': $route.path == '/commands' }" href="/commands">Commands</b-nav-item>
-        <b-nav-item id="item" :class="{ 'active': $route.path == '/contact' }" href="/contact">Contact</b-nav-item>
-        <b-nav-item id="item" :class="{ 'active': $route.path == '/stream' }" href="/stream">Stream</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+  <div class="navContainer">
+    <div class="container">
+      <b-navbar class="nav" toggleable="md">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="/">
+          <img class="logo" src="../assets/images/Profile.png" alt="logo">
+        </b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav pills>
+            <b-nav-item id="item" :class="{ 'active': $route.path == '/schedule' }" href="/schedule">Schedule</b-nav-item>
+            <b-nav-item id="item" :class="{ 'active': $route.path == '/commands' }" href="/commands">Commands</b-nav-item>
+            <b-nav-item id="item" :class="{ 'active': $route.path == '/contact' }" href="/contact">Contact</b-nav-item>
+            <b-nav-item id="item" :class="{ 'active': $route.path == '/stream' }" href="/stream">Stream</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </div>
 </template>
 
 <script>
